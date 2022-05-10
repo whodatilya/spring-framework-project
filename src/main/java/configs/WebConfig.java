030e352a2bfe3abd3a858f5a8c4316ae8e97ea1b
Ilya.Gordeev <gordeevilyas@yandex.ru>
2022-05-10 20:58:59 +0300

    Branch for Samat

--- src/main/java/configs/WebConfig.java
+++ src/main/java/configs/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
