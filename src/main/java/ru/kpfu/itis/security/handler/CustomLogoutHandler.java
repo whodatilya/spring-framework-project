ef5adeefed1de4e779d45e8d8048a02ad4e9cebc
Gordeev.Ilya <gordeevilyas@yandex.ru>
2022-05-14 12:23:24 +0300

    Delete old files and add custom logout

--- src/main/java/ru/kpfu/itis/security/handler/CustomLogoutHandler.java
+++ src/main/java/ru/kpfu/itis/security/handler/CustomLogoutHandler.java
@@ -1,17 +1,33 @@
 package ru.kpfu.itis.security.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
+import ru.kpfu.itis.models.Auth;
+import ru.kpfu.itis.repositories.AuthRepository;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component
+@Component("customLogoutHandler")
 public class CustomLogoutHandler implements LogoutHandler {
+
+    @Autowired
+    private AuthRepository authRepository;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie: cookies) {
+            if (cookie.getName().equals("Auth")){
+                Auth value = authRepository.findByCookieValue(cookie.getValue()).get();
+                authRepository.delete(value);
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+            }
+        }
     }
 }
--- src/main/java/ru/kpfu/itis/services/old/CommentService.java
+++
@@ -1,8 +0,0 @@
-//package ru.kpfu.itis.services;
-//
-//import forms.CommentForm;
-//import ru.kpfu.itis.models.Comment;
-//
-//public interface CommentService {
-//    Comment createNew(CommentForm commentForm);
-//}
